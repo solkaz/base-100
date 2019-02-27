890fe991fd950d9d5e88d1b18f1558df5d555919
Jeff Held <jheld135@gmail.com>
2019-02-26 19:23:37 -0600

    Get encoding working for number arrays

--- index.ts
+++ index.ts
@@ -2,17 +2,15 @@ export function decode(s: string): string {
   return "";
 }
 
-export function encode(s: string): string {
-  if (!s.length) {
-    return Buffer.from([240, 159, 143, 183]).toString();
-  }
-  const stringBuf = Buffer.from(s);
-  const writeBuf = Array(stringBuf.length * 4);
-  for (const [index, byte] of stringBuf.entries()) {
+export function encode(bytes: number[]): string {
+  const bytesLength = bytes.length;
+  const writeBuf = Array(bytesLength * 4);
+  for (let index = 0; index < bytesLength; index++) {
+    const byte = bytes[index];
     writeBuf[4 * index + 0] = 0xf0;
     writeBuf[4 * index + 1] = 0x9f;
     writeBuf[4 * index + 2] = ((byte + 55) >> 6) + 143;
-    writeBuf[4 * index + 3] = ((byte + 55) & 0x3f) + 143;
+    writeBuf[4 * index + 3] = ((byte + 55) & 0x3f) + 128;
   }
-  return Buffer.from(writeBuf.concat([240, 159, 143, 183])).toString();
+  return Buffer.from(writeBuf).toString();
 }
